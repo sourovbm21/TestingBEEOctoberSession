7fadbf88b930e23e59ac9ce6b6ad75c1d576671f
sourov <sourovbm21@gmail.com>
2022-10-16 19:23:27 +0100

    Our First Git

---
+++ src/main/java/ClassOne.java
@@ -0,0 +1,31 @@
+public class ClassOne {
+
+    public static void main(String[] args) {
+     //Data Type
+        int x = 880171523 ;//32 bit(-2*31 to 2*31-1)
+        int y = 5 ;
+        char a= 'q';
+        char b ='r';
+         String    s ="My Name is Sourov";
+         String j ="Java";
+         boolean t = true;
+         boolean t1= false;
+        float f1 = 234.5f;
+        double d1 = 12.3;
+        long l =1000000000;//62 bit (-2*63 to +2*63 )
+        System.out.println("$This is mobile number :--> "+x);//This is mobile number  880171523
+        System.out.println("$This is a long value :--> "+l);
+        System.out.println("$This is a long value :--> "+l);
+        System.out.println("$This is a long value :--> "+l);
+        System.out.println("$This is a long value :--> "+l);
+        System.out.println("$This is a long value :--> "+l);
+        System.out.println("$This is a long value :--> "+l);
+
+        //System.out.print("");
+
+    }
+
+
+
+
+}
