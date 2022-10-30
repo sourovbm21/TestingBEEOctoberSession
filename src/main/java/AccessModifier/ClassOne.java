fde860f9f0405f6258b49bd49d4d40ae266137cb
sourov <sourovbm21@gmail.com>
2022-10-30 16:12:02 +0000

    Adding File

--- src/main/java/AccessModifier/ClassOne.java
+++ src/main/java/AccessModifier/ClassOne.java
@@ -1,33 +1,21 @@
 package AccessModifier;
 public class ClassOne {
 
-    public static void main(String[] args) {
-
-
-       // System.out.println(c);
-        sum();
-        sum1();
-        System.out.println("This is from sum1 :  "+sum1());
-        sum2();
-        System.out.println("This is from sum2 :  "+sum2());
-        one(100,200);
-        System.out.println("This is from One :  "+one(1000,2020));
-        System.out.println("This is from Two :  "+two(1000,2020,2));
-    }
-
-   public static void sum(){
+    int x =100;
+    String s = "sourov";
+    public  void sum(){
        int s=3;
        int b =5;
        int  c = s+b;//8
       System.out.println("This is from sum method: "+c);
   }
-    public static int sum1(){
+    public  int sum1(){
         int s=5;
         int b =5;
         int  c = s+b;//10
         return c;
     }
-    public static boolean sum2(){
+    public  boolean sum2(){
         int s = 5;
         int b = 15;
         if (s == b){
@@ -35,23 +23,27 @@ public class ClassOne {
         }else {
             return false;
         }
-
     }
-    public static int sum3(){
+    public  int sum3(){
         int s=5;
         int b =5;
         int  c = s+b;//10
         return c;
     }
-    public static int  one(int x,int y ){
+    public  int  one(int x,int y ){
         int c = x+y;
         return c;
     }
-    public static int  two(int x,int y,int z ){
+    public  int  two(int x,int y,int z ){
         int c = x+y/z;
         return c;
     }
+    public  void three(int x, String s,boolean b){
+        System.out.println("Th");
+    }
+    public  void four(int x, String s,boolean b,double d){
 
+    }
 
 
 
---
+++ src/main/java/AccessModifier/ClassTwo.java
@@ -0,0 +1,21 @@
+package AccessModifier;
+
+public class ClassTwo {
+
+
+    public static void main(String[] args) {
+        // classname referencevariable = new classname();
+
+        ClassOne classOne = new ClassOne();
+        classOne.sum();
+        classOne.four(1,"s",true,2.3);
+        classOne.sum1();
+        classOne.three(1,"sssdfgsdhsghnthnsrgs",true);
+        System.out.println(classOne.x);
+
+
+
+    }
+
+
+}
