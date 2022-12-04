e04184f9995046c99a5a362555b76feed8821555
sourov <sourovbm21@gmail.com>
2022-12-04 15:46:52 +0000

    Todays Selenium Class

--- src/test/java/Selenium/ClassOne.java
+++ src/test/java/Selenium/ClassOne.java
@@ -2,7 +2,9 @@ package Selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -22,9 +24,9 @@ public class ClassOne {
 
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
-       // driver.get("https://www.amazon.co.uk");
+        // driver.get("https://www.amazon.co.uk");
 
-      driver.get("https://rahulshettyacademy.com/AutomationPractice/");
+        driver.get("https://rahulshettyacademy.com/AutomationPractice/");
         //Locators( Tagname, Attribute, Value
         //<input type="text" id="autocomplete" class="inputs ui-autocomplete-input" placeholder="Type to Select Countries" autocomplete="off">
            /*
@@ -38,24 +40,34 @@ public class ClassOne {
            Tagname
             */
 //
- // driver.findElement(By.xpath("//input[@placeholder='Type to Select Countries']")).sendKeys("Fahmida");
+        // driver.findElement(By.xpath("//input[@placeholder='Type to Select Countries']")).sendKeys("Fahmida");
         driver.findElement(By.xpath("//input[@value='radio3']")).click();
-   driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Anika");
-   //driver.findElement(By.id("autocomplete")).sendKeys("Rubina");
-  //driver.findElement(By.className("inputs ui-autocomplete-input")).sendKeys("Sourov");
+        driver.findElement(By.cssSelector("input[type='text']")).sendKeys("Anika");
+        //driver.findElement(By.id("autocomplete")).sendKeys("Rubina");
+        //driver.findElement(By.className("inputs ui-autocomplete-input")).sendKeys("Sourov");
 //   driver.findElement(By.ByLinkText());
         driver.findElement(By.xpath("//input[@id='checkBoxOption1']")).click();
         Thread.sleep(2000l);
         driver.findElement(By.xpath("//input[@id='checkBoxOption2']")).click();
+        //Alert
+        driver.findElement(By.xpath("//input[@id='confirmbtn']")).click();
+        Thread.sleep(3000);
+      String  alertmsg =  driver.switchTo().alert().getText();
+        System.out.println(alertmsg);
+        driver.switchTo().alert().accept();
+
+       String pageHeader =  driver.findElement(By.xpath("//h1")).getText();
+        System.out.println(pageHeader);
+// How to Scroll
+       // WebElement scrollTo =  driver.findElement(By.xpath("//button[@id='mousehover']"));
+        WebElement sc = driver.findElement(By.xpath("//a[text()='Latest News']"));
+        JavascriptExecutor js = ( JavascriptExecutor ) driver;
+        js.executeScript("arguments[0].scrollIntoView();",sc);
+
 
 
         Thread.sleep(5000l);
-        driver.quit();
-
-
-
-
-
+       // driver.quit();
 
 
     }
