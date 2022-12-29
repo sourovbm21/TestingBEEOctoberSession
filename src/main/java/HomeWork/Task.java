779c8656965dda979a83f3f9f24ab08483ad867b
sourov <sourovbm21@gmail.com>
2022-12-29 17:15:12 +0000

    Adding New Task

--- src/main/java/HomeWork/Task.java
+++ src/main/java/HomeWork/Task.java
@@ -18,6 +18,52 @@ public class Task {
 	 * 10.Print 1 to 10 using while loop;
 	 *
      */
+	/*
+	 * Problem:- 11. Given a string and a non-negative int n, return a larger string
+	 * that is n copies of the original string. Example below : stringTimes("Hi", 2)
+	 * → "HiHi" stringTimes("Hi", 3) → "HiHiHi" stringTimes("Hi", 1) → "Hi"
+	 */
+
+	/*
+	 * Problem:-12 Given a string, we'll say that the front is the first 3 chars of
+	 * the string, If the string length is less than 3, the front is whatever is
+	 * there, Return a new string which is 3 copies of the front. Example below :
+	 * front3("Java") → "JavJavJav" front3("Chocolate") → "ChoChoCho" front3("abc")
+	 * → "abcabcabc"
+	 */
+
+
+	/*
+	 * Problem:-13 Given a string, take the last char and return a new string with
+	 * the last char added at the front and back, so "cat" yields "tcatt". The
+	 * original string will be length 1 or more. backAround("cat") → "tcatt"
+	 * backAround("Hello") → "oHelloo" backAround("a") → "aaa
+	 */
+
+
+	/*
+	 * Problem-14 Return true if the given non-negative number is a multiple of 3 or
+	 * a multiple of 5. Use the % "mod" operator or35(3) → true or35(10) → true
+	 * or35(8) → false
+	 */
+
+
+	/*
+	 * Problem :-15 Given a string, take the first 2 chars and return the string
+	 * with the 2 chars added at both the front and back, so "kitten"
+	 * yields"kikittenki". If the string length is less than 2, use whatever chars
+	 * are there. front22("kitten") → "kikittenki" front22("Ha") → "HaHaHa"
+	 * front22("abc") → "ababcab"
+	 */
+
+	/*Problem :-16
+     * Given a string, if the string "del" appears starting at index 1,
+     * return a string where that "del" has been deleted. Otherwise, return the string unchanged.
+       delDel("adelbc") → "abc"
+       delDel("adelHello") → "aHello"
+       delDel("adedbc") → "adedbc"
+     */
+
 
 	public static void main(String[] args) {
 
