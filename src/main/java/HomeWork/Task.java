942d64d81f8c76037d77de2d8cbd308cbcd2d06d
sourov <sourovbm21@gmail.com>
2023-01-01 13:52:56 +0000

    Adding Nested Frame Class

--- src/main/java/HomeWork/Task.java
+++ src/main/java/HomeWork/Task.java
@@ -23,7 +23,6 @@ public class Task {
 	 * that is n copies of the original string. Example below : stringTimes("Hi", 2)
 	 * → "HiHi" stringTimes("Hi", 3) → "HiHiHi" stringTimes("Hi", 1) → "Hi"
 	 */
-
 	/*
 	 * Problem:-12 Given a string, we'll say that the front is the first 3 chars of
 	 * the string, If the string length is less than 3, the front is whatever is
@@ -31,23 +30,17 @@ public class Task {
 	 * front3("Java") → "JavJavJav" front3("Chocolate") → "ChoChoCho" front3("abc")
 	 * → "abcabcabc"
 	 */
-
-
 	/*
 	 * Problem:-13 Given a string, take the last char and return a new string with
 	 * the last char added at the front and back, so "cat" yields "tcatt". The
 	 * original string will be length 1 or more. backAround("cat") → "tcatt"
 	 * backAround("Hello") → "oHelloo" backAround("a") → "aaa
 	 */
-
-
 	/*
 	 * Problem-14 Return true if the given non-negative number is a multiple of 3 or
 	 * a multiple of 5. Use the % "mod" operator or35(3) → true or35(10) → true
 	 * or35(8) → false
 	 */
-
-
 	/*
 	 * Problem :-15 Given a string, take the first 2 chars and return the string
 	 * with the 2 chars added at both the front and back, so "kitten"
@@ -55,7 +48,6 @@ public class Task {
 	 * are there. front22("kitten") → "kikittenki" front22("Ha") → "HaHaHa"
 	 * front22("abc") → "ababcab"
 	 */
-
 	/*Problem :-16
      * Given a string, if the string "del" appears starting at index 1,
      * return a string where that "del" has been deleted. Otherwise, return the string unchanged.
---
+++ src/test/java/Selenium/NestedFrames.java
@@ -0,0 +1,12 @@
+package Selenium;
+
+public class NestedFrames {
+
+
+
+
+
+
+
+
+}
